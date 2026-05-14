7caa7b1c681c66fc50e2619e025b8689ffec137a
River Mochi <97197236+River-Mochi@users.noreply.github.com>
2026-05-13 22:14:03 -0700

    add new Icons for GTL button and SIP

--- src/UI/src/index.tsx
+++ src/UI/src/index.tsx
@@ -3,7 +3,7 @@ import mod from "../mod.json";
 import { NotificationPanel } from "./mods/NotificationPanel/NotificationPanel";
 import { EntryButton } from "./mods/EntryButton/EntryButton";
 import { VanillaComponentResolver } from "./mods/VanillaComponentResolver/VanillaComponentResolver";
-import "../images/CityWatchdogIcon_colored.svg";
+import "../images/Yellow_Warning.svg";
 
 const register: ModRegistrar = (moduleRegistry) => {
     VanillaComponentResolver.setRegistry(moduleRegistry);
